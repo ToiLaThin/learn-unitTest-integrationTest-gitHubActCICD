422172f85670156b297ac38aacb863a1c5ba20e7
ToiLaThin <thinh@gmail.com>
2024-01-19 07:58:47 +0700

    init commit

---
+++ ApiUnitTesting/ApiUnitTesting.Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ApiUnitTesting.Api.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiUnitTesting.Api.Data
+{
+    public class AppDbContext: DbContext
+    {
+        DbSet<Employee> Employees { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
